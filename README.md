af6c7cc0b0afec0544cada1763e45e9195b63f44
Michael Vo <michael.minh.vo@gmail.com>
2020-07-26 12:21:19 -0400

    working version cleaned up

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # cloud-correlation
 A cloud function to calculate the correlation matrix
+
+# Test locally
+
+Install functions-framework from google
+`pip3 install functions-framework`
+
+`functions-framework --target hello_http`
+
+`localhost:8080`
---
+++ main.py
@@ -0,0 +1,21 @@
+import logging
+import requests
+import pandas as pd
+from flask import Flask
+from flask_cors import cross_origin
+from flask import make_response
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
+def helloWorld(request):
+    if request.method == 'POST':
+        df = pd.read_csv(request.files.get('file'))
+        correlation = df.corr(method='pearson')
+        resp = make_response(correlation.to_csv())
+        resp.headers["Content-Disposition"] = "attachment; filename=export.csv"
+        resp.headers["Content-Type"] = "text/csv"
+        return resp
+    return "Ok"
